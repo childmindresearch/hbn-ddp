f65940f17d1bac7745e1fcd7e9e8b8e2327f780d
maya-roberts <maya36247@gmail.com>
2025-01-07 11:55:33 -0500

    new data corrections

--- src/hbn-preprocess/DiagnosticDataCleaning.py
+++ src/hbn-preprocess/DiagnosticDataCleaning.py
@@ -1,7 +1,5 @@
 """Script to fix ClinicianConsensusDiagnosis Data."""
 
-import itertools
-
 import pandas as pd
 
 
@@ -49,49 +47,69 @@ def data_clean(hbn_data_path: str) -> None:
     dxes.sort()
 
     # correct errors in certainty data
-    for i, n in itertools.product(range(0, len(df)), dx_ns):
-        # if any of confirmed, presum, or RC are 1, if bybx is NULL,
-        # by history should be 0
-        if any(
-            [
-                df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Confirmed"] == 1,
-                df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Presum"] == 1,
-                df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_RC"] == 1,
-                df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_RuleOut"] == 1,
-            ]
-        ):
-            if pd.isna(df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_ByHx"]):
-                df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_ByHx"] = 0
-        # if confirmed and presum are NULL, and RC, Rule-Out and ByHx are 0,
-        # confirmed should be 1
-        if all(
-            [
-                pd.isna(
-                    df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Confirmed"]
-                ),
-                pd.isna(df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Presum"]),
-                df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_RC"] == 0,
-                df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_RuleOut"] == 0,
-                df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_ByHx"] == 0,
-            ]
-        ):
-            df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Confirmed"] = 1
-            df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Presum"] = 0
-        # if confirmed and presum are NULL, and one of RC, Rule-Out or By History are 1,
-        # confirmed and presum should be 0
-        if pd.isna(
-            df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Confirmed"]
-        ) and pd.isna(df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Presum"]):
+    for n in dx_ns:
+        for i in range(0, len(df)):
+            # if any of confirmed, presum, or RC are 1, if ByHx is NULL,
+            # by history should be 0
             if any(
                 [
+                    df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Confirmed"]
+                    == 1,
+                    df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Presum"] == 1,
                     df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_RC"] == 1,
                     df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_RuleOut"] == 1,
-                    df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_ByHx"] == 1,
                 ]
             ):
-                df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Confirmed"] = 0
+                if pd.isna(df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_ByHx"]):
+                    df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_ByHx"] = 0
+            # if confirmed and presum are NULL, and RC, Rule-Out and ByHx are 0,
+            # confirmed should be 1
+            if all(
+                [
+                    pd.isna(
+                        df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Confirmed"]
+                    ),
+                    pd.isna(
+                        df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Presum"]
+                    ),
+                    df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_RC"] == 0,
+                    df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_RuleOut"] == 0,
+                    df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_ByHx"] == 0,
+                ]
+            ):
+                df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Confirmed"] = 1
                 df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Presum"] = 0
-
+            # if confirmed and presum are NULL, and one of RC, RuleOut or ByHx are 1,
+            # confirmed and presum should be 0
+            if pd.isna(
+                df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Confirmed"]
+            ) and pd.isna(df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Presum"]):
+                if any(
+                    [
+                        df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_RC"] == 1,
+                        df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_RuleOut"]
+                        == 1,
+                        df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_ByHx"] == 1,
+                    ]
+                ):
+                    df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Confirmed"] = 0
+                    df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Presum"] = 0
+            # if ByHx, Time should be Null
+            if df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_ByHx"] == 1:
+                df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Time"] = pd.NA
+            # Past Doc should only be noted if ByHx is 1 or Time is 2
+            if all(
+                [
+                    df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_ByHx"] != 1,
+                    df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Time"] != 2,
+                ]
+            ):
+                df.at[i, "Diagnosis_ClinicianConsensus,DX_" + n + "_Past_Doc"] = pd.NA
+        # Remove "New" column
+        df = df.drop(columns=["Diagnosis_ClinicianConsensus,DX_" + n + "_New"])
+        # Remove remission columns (Remission and Partial Remission)
+        df = df.drop(columns=["Diagnosis_ClinicianConsensus,DX_" + n + "_Rem"])
+        df = df.drop(columns=["Diagnosis_ClinicianConsensus,DX_" + n + "_PRem"])
     save_path = hbn_data_path.replace(".csv", "_cleaned.csv")
     df.to_csv(save_path, index=False)
 
