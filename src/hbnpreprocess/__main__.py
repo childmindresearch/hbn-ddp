05152b4fc1c8c538cc36afc3256229c9b26e0158
maya-roberts <maya36247@gmail.com>
2025-05-23 13:50:36 -0400

    cli

--- src/hbnpreprocess/__main__.py
+++ src/hbnpreprocess/__main__.py
@@ -1,17 +1,28 @@
 """Main script for CLI run."""
 
+import typer
+
 from hbnpreprocess.hbn_data import HBNData
 from hbnpreprocess.prompting import Interactive
 
-# CLI run
-args = Interactive.prompt()
-
-
-HBNData.process(
-    input_path=args["input_path"],
-    output_path=args["output_path"],
-    by=args["by"],
-    certainty_filter=args["certainty_filter"],
-    include_details=args["include_details"],
-    viz=args["viz"],
+app = typer.Typer(
+    help="CLI for preprocessing HBN diagnostic data.",
 )
+
+
+@app.command()
+def main() -> None:
+    """Main function for CLI run."""
+    args = Interactive.prompt()
+    HBNData.process(
+        input_path=args["input_path"],
+        output_path=args["output_path"],
+        by=args["by"],
+        certainty_filter=args["certainty_filter"],
+        include_details=args["include_details"],
+        viz=args["viz"],
+    )
+
+
+if __name__ == "__main__":
+    app()
